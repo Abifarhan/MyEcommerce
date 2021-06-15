571f52b7ade12aa241b8b731927c394c9e839d55
abiFarhan <abifarhan04@gmail.com>
2021-06-15 23:46:34 +0700

    Implement code in SplashActivity.kt

--- app/src/main/java/com/abifarhan/myecommerce/view/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/splash/SplashActivity.kt
@@ -1,12 +1,27 @@
 package com.abifarhan.myecommerce.view.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import com.abifarhan.myecommerce.R
+import com.abifarhan.myecommerce.view.ui.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            },3000
+        )
     }
 }
