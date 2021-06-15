0b41cc41fd9e33957ccd5be37f1985801169796a
abiFarhan <abifarhan04@gmail.com>
2021-06-15 23:27:51 +0700

    Add SplashActivity.kt and sdp third library

---
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.abifarhan.myecommerce.view.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abifarhan.myecommerce.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
