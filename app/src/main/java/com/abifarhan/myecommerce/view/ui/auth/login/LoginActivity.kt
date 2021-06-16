45b26a150634b46a2c4ede216aa1c0f965533df3
abiFarhan <abifarhan04@gmail.com>
2021-06-16 23:42:47 +0700

    add rules about Android version for Full Screen for layout LoginActivity.kt run properly to every device

--- app/src/main/java/com/abifarhan/myecommerce/view/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/auth/login/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.abifarhan.myecommerce.view.ui.auth.login
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.view.WindowInsets
 import android.view.WindowManager
 import com.abifarhan.myecommerce.R
 import com.abifarhan.myecommerce.databinding.ActivityLoginBinding
@@ -15,6 +17,9 @@ class LoginActivity : AppCompatActivity() {
         _binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
