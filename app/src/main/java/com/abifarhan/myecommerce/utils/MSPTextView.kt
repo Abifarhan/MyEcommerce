56577a69d7e088a90eb32b805656691cf0d2a736
abiFarhan <abifarhan04@gmail.com>
2021-06-16 23:17:18 +0700

    Add progress customView for TextView

--- app/src/main/java/com/abifarhan/myecommerce/utils/MSPTextView.kt
+++ app/src/main/java/com/abifarhan/myecommerce/utils/MSPTextView.kt
@@ -1,4 +1,20 @@
 package com.abifarhan.myecommerce.utils
 
-class MSPTextView {
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class MSPTextView(context: Context, attributeSet: AttributeSet) :
+    AppCompatTextView(context, attributeSet) {
+
+        init {
+            applyFont()
+        }
+
+    private fun applyFont() {
+        val typeFace: Typeface =
+            Typeface.createFromAsset(context.assets,"Montserrat-Regular.ttf")
+        typeface = typeface
+    }
 }
