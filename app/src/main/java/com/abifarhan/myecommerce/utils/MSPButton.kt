c13d3a081d22b93b5ec6e0c2aa20ff75c00da510
abiFarhan <abifarhan04@gmail.com>
2021-06-16 23:18:50 +0700

    Add progress customView for Button

--- app/src/main/java/com/abifarhan/myecommerce/utils/MSPButton.kt
+++ app/src/main/java/com/abifarhan/myecommerce/utils/MSPButton.kt
@@ -1,4 +1,19 @@
 package com.abifarhan.myecommerce.utils
 
-class MSPButton {
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class MSPButton(context: Context, attributeSet: AttributeSet) :
+    AppCompatButton(context, attributeSet) {
+
+        init {
+            applyFont()
+        }
+
+    private fun applyFont() {
+        val typeFace = Typeface.createFromAsset(context.assets,"Montserrat-Regular.ttf")
+        typeface = typeFace
+    }
 }
