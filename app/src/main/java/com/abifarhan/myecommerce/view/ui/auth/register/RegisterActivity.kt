6a7858e65a4fb12912714c300890d4bae106373f
abiFarhan <abifarhan04@gmail.com>
2021-06-16 23:56:43 +0700

    show layout for RegisterActivity.kt with properly view

--- app/src/main/java/com/abifarhan/myecommerce/view/ui/auth/register/RegisterActivity.kt
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/auth/register/RegisterActivity.kt
@@ -1,15 +1,28 @@
 package com.abifarhan.myecommerce.view.ui.auth.register
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.abifarhan.myecommerce.R
 import com.abifarhan.myecommerce.databinding.ActivityRegisterBinding
+import com.abifarhan.myecommerce.view.ui.base.BaseActivity
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     private var _binding: ActivityRegisterBinding? = null
     private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        _binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
     }
 }
