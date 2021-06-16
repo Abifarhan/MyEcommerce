6dc4a8eadc9ef2ad02cc975f5d94616f48108073
abiFarhan <abifarhan04@gmail.com>
2021-06-16 23:36:35 +0700

    add file for auth

---
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.abifarhan.myecommerce.view.ui.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abifarhan.myecommerce.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
