4ebeb38438639d5f6774603a6194bfac80bcb877
abiFarhan <abifarhan04@gmail.com>
2021-06-16 23:39:25 +0700

    add viewBinding for layout LoginActivity.kt

--- app/src/main/java/com/abifarhan/myecommerce/view/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/auth/login/LoginActivity.kt
@@ -3,8 +3,11 @@ package com.abifarhan.myecommerce.view.ui.auth.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.abifarhan.myecommerce.R
+import com.abifarhan.myecommerce.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private var _binding: ActivityLoginBinding? = null
+    private val binding get() = _binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
