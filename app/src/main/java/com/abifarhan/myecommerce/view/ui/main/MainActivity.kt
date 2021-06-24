76b9982848220baf821aea72a9f7c6518a1008fe
abiFarhan <abifarhan04@gmail.com>
2021-06-24 22:31:14 +0700

    Design UserProfile part 1

--- app/src/main/java/com/abifarhan/myecommerce/view/ui/main/MainActivity.kt
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/main/MainActivity.kt
@@ -9,15 +9,15 @@ import com.abifarhan.myecommerce.utils.Constants
 
 class MainActivity : AppCompatActivity() {
     private var binding: ActivityMainBinding? = null
-//    private val binding = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)!!
+        setContentView(binding!!.root)
 
         val sharedPreferences =
-            getSharedPreferences(Constants.MYSHOPPAL_PREFERENCES,
+            getSharedPreferences(
+                Constants.MYSHOPPAL_PREFERENCES,
             Context.MODE_PRIVATE)
 
         val username = sharedPreferences.getString(
