1ce8f6ca7dae67dc48e12fa970fbb188b5210398
abiFarhan <abifarhan04@gmail.com>
2021-06-25 11:33:00 +0700

    Update Profile for take picture

--- app/src/main/java/com/abifarhan/myecommerce/utils/Constants.kt
+++ app/src/main/java/com/abifarhan/myecommerce/utils/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
     const val MYSHOPPAL_PREFERENCES: String = "MyShopPalPrefs"
     const val LOGGED_IN_USERNAME: String = "logged_in_username"
     const val EXTRA_USER_DETAILS: String = "extra_user_details"
+    const val READ_STORAGE_PERMISSION_CODE = 2
+
 }
--- app/src/main/java/com/abifarhan/myecommerce/view/ui/profile/UserProfileActivity.kt
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/profile/UserProfileActivity.kt
@@ -1,13 +1,20 @@
 package com.abifarhan.myecommerce.view.ui.profile
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.abifarhan.myecommerce.R
 import com.abifarhan.myecommerce.databinding.ActivityUserProfileBinding
 import com.abifarhan.myecommerce.model.User
 import com.abifarhan.myecommerce.utils.Constants
+import com.abifarhan.myecommerce.view.ui.base.BaseActivity
+import java.util.jar.Manifest
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BaseActivity(), View.OnClickListener {
     private lateinit var binding: ActivityUserProfileBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +23,8 @@ class UserProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         var userDetail: User = User()
-        if (intent.hasExtra(Constants.EXTRA_USER_DETAILS)) {
+        if (intent.hasExtra(Constants.EXTRA_USER_DETAILS))
+        {
             userDetail = intent.getParcelableExtra(
                 Constants.EXTRA_USER_DETAILS
             )!!
@@ -35,6 +43,51 @@ class UserProfileActivity : AppCompatActivity() {
         binding!!.etEmail.apply {
             isEnabled = false
             setText(userDetail.email)
+        }
+
+        binding.ivUserPhoto.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+
+                R.id.iv_user_photo -> {
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        showErrorSnackBar("You already have the storage permission.", false)
+                    }
+                    else{
+                        ActivityCompat.requestPermissions(
+                            this,
+                            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                            Constants.READ_STORAGE_PERMISSION_CODE
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showErrorSnackBar("The storage permission is granted.", false)
+            }else{
+                Toast.makeText(
+                    this,
+                    resources.getString(R.string.read_storage_permission_denied),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
