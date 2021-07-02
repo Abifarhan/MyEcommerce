e7d71a47e1956ff872e586412e49801820c9bb4a
abiFarhan <abifarhan04@gmail.com>
2021-07-02 20:52:23 +0700

    Add DashboardActivity with the components

---
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/dashboard/DashBoardActivity.kt
@@ -0,0 +1,36 @@
+package com.abifarhan.myecommerce.view.ui.dashboard
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.abifarhan.myecommerce.R
+import com.abifarhan.myecommerce.databinding.ActivityDashBoardBinding
+
+class DashBoardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashBoardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDashBoardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_dash_board)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/dashboard/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.abifarhan.myecommerce.view.ui.dashboard.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
