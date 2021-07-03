2508ee9ce4ec0966e63d1d3ab601f7edf21af1ef
abiFarhan <abifarhan04@gmail.com>
2021-07-03 07:37:37 +0700

    Proceed the setting Activity with the Toolbar

--- app/src/main/java/com/abifarhan/myecommerce/view/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/settings/SettingsActivity.kt
@@ -8,5 +8,6 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+//        actionBar?.hide()
     }
 }
