da1da3b1de4fc8c2089527cc970ffe0697a2b8e0
abiFarhan <abifarhan04@gmail.com>
2021-07-03 21:44:06 +0700

    Proceed the toolbar

--- app/src/main/java/com/abifarhan/myecommerce/view/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/abifarhan/myecommerce/view/ui/settings/SettingsActivity.kt
@@ -3,11 +3,35 @@ package com.abifarhan.myecommerce.view.ui.settings
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.abifarhan.myecommerce.R
+import com.abifarhan.myecommerce.databinding.ActivitySettingsBinding
+import com.abifarhan.myecommerce.view.ui.base.BaseActivity
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : BaseActivity() {
+    private var _binding: ActivitySettingsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        _binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupActionBar()
 //        actionBar?.hide()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbarSettingsActivity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(
+                R.drawable.ic_baseline_arrow_back_ios_24
+            )
+        }
+
+        binding.toolbarSettingsActivity.
+                setNavigationOnClickListener {
+                    onBackPressed()
+                }
     }
 }
