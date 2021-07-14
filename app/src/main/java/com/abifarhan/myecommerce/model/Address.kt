346ae58f9fab3ad9c60fff2aa3286de5291a1b23
abiFarhan <abifarhan04@gmail.com>
2021-07-14 15:04:00 +0700

    Proceed the Products operation part 19

---
+++ app/src/main/java/com/abifarhan/myecommerce/model/Address.kt
@@ -0,0 +1,19 @@
+package com.abifarhan.myecommerce.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Address(
+    val userId: String = "",
+    val name: String = "",
+    val mobileNumber: String = "",
+
+    val address: String = "",
+    val zipCode: String = "",
+    val additionalNote: String = "",
+
+    val type: String = "",
+    val otherDetails: String = "",
+    val id: String = ""
+): Parcelable
